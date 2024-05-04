15b5450bf2a56e8ade26593f44fe27ae8b82a1be
Guillermo Mercado <guillermo.mercado26@gmail.com>
2024-05-04 11:56:51 -0400

    lo q izo el profe

--- clase04-05/assets/js/script-Guillermo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    while (true){
-        alert("wa");
-        alert("FUAP!");
-        alert("suinga abanica");
-        alert("alimenta a tu familia con");}
-    
-    
-})
