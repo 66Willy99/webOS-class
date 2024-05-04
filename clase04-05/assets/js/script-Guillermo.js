37f3ef84d3d4f7112a0b3c350a208d5bc31a3fa4
Guillermo Mercado <guillermo.mercado26@gmail.com>
2024-05-04 11:38:26 -0400

    se ordena el repo 2

---
+++ clase04-05/assets/js/script-Guillermo.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    while (true){
+        alert("wa");
+        alert("FUAP!");
+        alert("suinga abanica");
+        alert("alimenta a tu familia con");}
+    
+    
+})
